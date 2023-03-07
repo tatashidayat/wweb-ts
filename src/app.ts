fd7d481d5d06cf0b3c8655771a980010783805fd
Tatas Hidayat <tatas.hidayat@tech.jago.com>
2023-03-07 10:37:01 +0700

    chore: initial commit

---
+++ src/app.ts
@@ -0,0 +1,146 @@
+import * as express from 'express';
+import * as qrcodeTerminal from 'qrcode-terminal';
+import {Client, LocalAuth, Message, WAState} from 'whatsapp-web.js';
+import {WhatsAppService, WhatsAppServiceState} from './service/WhatsAppService';
+
+import socketIO = require('socket.io');
+import qrcode = require('qrcode');
+import http = require('http');
+
+const start = async (): Promise<void> => {
+  const client = new Client({
+    restartOnAuthFail: true,
+    puppeteer: {
+      headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-accelerated-2d-canvas',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process', // <- this one doesn't works in Windows
+        '--disable-gpu',
+      ],
+    },
+    authStrategy: new LocalAuth(),
+  });
+
+  const waService = new WhatsAppService(client);
+
+  waService.on(WhatsAppServiceState.UNINITIALIZED, () => {
+    console.log('WA Uninitialized');
+  });
+  waService.on(WhatsAppServiceState.INITIALIZING, () => {
+    console.log('WA Initialiazing');
+  });
+  waService.on(WhatsAppServiceState.INITIALIZED, () => {
+    console.log('WA Initialized');
+  });
+  waService.on(WhatsAppServiceState.QR, (qr: string) => {
+    console.log('QR Ready, Please scan!');
+    qrcodeTerminal.generate(qr, {small: true});
+  });
+  waService.on(WhatsAppServiceState.AUTHENTICATED, () => {
+    console.log('WA Authenticated, Please wait till Ready!');
+  });
+  waService.on(WhatsAppServiceState.READY, () => {
+    console.log('WA Ready!');
+  });
+  waService.on(WhatsAppServiceState.AUTH_FAILED, reason => {
+    console.log('WA Authentication Failed', reason);
+  });
+  waService.on(WhatsAppServiceState.DISCONNECTED, reason => {
+    console.log('WA Disconnected', reason);
+  });
+
+  waService.on(WhatsAppServiceState.MESSAGE, (message: Message) => {
+    console.log(`Recieve Message from:${message.from} message:${message.body}`);
+  });
+
+  startServer(waService);
+  await waService.init();
+};
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+const io = new socketIO.Server(server);
+
+const startServer = (waService: WhatsAppService) => {
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+
+  io.on('connection', socket => {
+    socket.emit('message', 'Connecting...');
+
+    const onQR = (qr: string) => {
+      qrcode.toDataURL(qr, (err, url) => {
+        socket.emit('qr', url);
+        socket.emit('message', 'QR Code received, scan please!');
+      });
+    };
+
+    const onReady = () => {
+      socket.emit('ready', 'Whatsapp is ready!');
+      socket.emit('message', 'Whatsapp is ready!');
+    };
+
+    const onAuthenticated = () => {
+      socket.emit('authenticated', 'Whatsapp is authenticated!');
+      socket.emit('message', 'Whatsapp is authenticated!');
+      console.log('AUTHENTICATED');
+    };
+
+    const onAuthFailed = (reason: string) => {
+      socket.emit('message', `Auth failure due to ${reason}, restarting...`);
+    };
+
+    const onDisconnected = (reason: WAState | 'NAVIGATION') => {
+      socket.emit(`message', 'Whatsapp is disconnected!, reason ${reason}`);
+    };
+
+    waService.on(WhatsAppServiceState.QR, onQR);
+    waService.on(WhatsAppServiceState.READY, onReady);
+    waService.on(WhatsAppServiceState.AUTHENTICATED, onAuthenticated);
+    waService.on(WhatsAppServiceState.AUTH_FAILED, onAuthFailed);
+    waService.on(WhatsAppServiceState.DISCONNECTED, onDisconnected);
+
+    const firstState = waService.state;
+
+    if (firstState === WhatsAppServiceState.UNINITIALIZED) {
+      waService.init();
+      return;
+    }
+
+    if (waService.qr) {
+      onQR(waService.qr);
+    }
+
+    switch (firstState) {
+      case WhatsAppServiceState.READY:
+      case WhatsAppServiceState.MESSAGE:
+        onReady();
+        break;
+      default:
+        break;
+    }
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile('index.html', {
+      root: __dirname,
+    });
+  });
+
+  server.listen(port, () => {
+    console.log('Server is started on port:', port);
+  });
+};
+
+start();
---
+++ src/service/WhatsAppService.ts
@@ -0,0 +1,74 @@
+import {EventEmitter} from 'events';
+import {Client, ClientSession, WAState} from 'whatsapp-web.js';
+
+export enum WhatsAppServiceState {
+  UNINITIALIZED = 'UNINITIALIZED',
+  INITIALIZING = 'INITIALIZING',
+  INITIALIZED = 'INITIALIZED',
+  QR = 'QR',
+  AUTHENTICATED = 'AUTHENTICATED',
+  AUTH_FAILED = 'AUTH_FAILED',
+  READY = 'READY',
+  MESSAGE = 'MESSAGE',
+  DISCONNECTED = 'DISCONNECTED',
+}
+
+export class WhatsAppService extends EventEmitter {
+  private _client: Client;
+  private _state: WhatsAppServiceState;
+  private _lastQR?: string;
+
+  constructor(client: Client) {
+    super();
+    this._client = client;
+    this._state = WhatsAppServiceState.UNINITIALIZED;
+
+    this._client.on('qr', (qr: string) => {
+      this._state = WhatsAppServiceState.QR;
+      this.emit(this._state, qr);
+      this._lastQR = qr;
+    });
+
+    this._client.on('ready', () => {
+      this._state = WhatsAppServiceState.READY;
+      this.emit(this._state);
+      this._lastQR = undefined;
+    });
+
+    this._client.on('authenticated', (session?: ClientSession) => {
+      this._state = WhatsAppServiceState.AUTHENTICATED;
+      this.emit(this._state, session);
+      this._lastQR = undefined;
+    });
+
+    this._client.on('auth_failure', (reason: string) => {
+      this._state = WhatsAppServiceState.AUTH_FAILED;
+      this.emit(this._state, reason);
+      this._lastQR = undefined;
+    });
+
+    this._client.on('disconnected', (reason: WAState | 'NAVIGATION') => {
+      this._state = WhatsAppServiceState.DISCONNECTED;
+      this.emit(this._state, reason);
+      this._client.destroy();
+      this._client.initialize();
+    });
+  }
+
+  get state(): WhatsAppServiceState {
+    return this._state;
+  }
+
+  get qr(): string | undefined {
+    return this._lastQR;
+  }
+
+  init = async (): Promise<void> => {
+    if (this._state === WhatsAppServiceState.READY) {
+      return;
+    }
+    this._state = WhatsAppServiceState.INITIALIZING;
+    await this._client.initialize();
+    this._state = WhatsAppServiceState.INITIALIZED;
+  };
+}
